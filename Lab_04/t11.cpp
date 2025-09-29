0ec5d56083d2a3d6ac87c2786afdf8688de55e7f
Yaroslava Krenevych <157513168+niancat286@users.noreply.github.com>
2025-09-29 22:39:02 +0300

    Update t11.cpp in Lab04

--- Lab_04/t11.cpp
+++ Lab_04/t11.cpp
@@ -2,13 +2,10 @@
 #include <math.h>
 
 int main() {
-    double x;
+    double x, sum_real = 0;
     int i = 0;
 
-    double sum = 0;
-    long long count = 0;
-    long long sum_int = 0;
-    double product = 1;
+    printf("Введення дійсних чисел (завершується, якщо введено 0):\n");
 
     while (1) {
         printf("a[%d]= ", i);
@@ -16,27 +13,44 @@ int main() {
 
         if (fabs(x) < 1e-12) break;
 
-        sum += x;
-
-        if ((long long)x == x) {
-            int xi = (int)x;
-            sum_int += xi;
-            product *= xi;
-            count++;
-        }
+        sum_real += x;
         i++;
     }
 
-    printf("\nСума введених дійсних чисел = %.2lf\n", sum);
+    printf("\nСума введених дійсних чисел = %.2lf\n", sum_real);
+
+    int y, count = 0;
+    long long sum_int = 0;
+    double product = 1;
+
+    printf("\nВведення цілих ненульових чисел (завершується при 0):\n");
+
+    while (1) {
+        printf("b[%d]= ", count);
+        if (scanf("%d", &y) == 1) {
+            if (y == 0) break; 
+
+            sum_int += y;
+            product *= y;
+            count++;
+        }
+        else  {
+            printf("Це НЕ ціле число (має дробову частину).\n");
+            sum_int -= y;
+            product /= y;
+            count--;
+            break;
+        }
+    }
 
     if (count > 0) {
         double avg_arith = (double)sum_int / count;
         double avg_geom = pow(product, 1.0 / count);
 
-        printf("Середнє арифметичне цілих чисел = %.2lf\n", avg_arith);
+        printf("\nСереднє арифметичне цілих чисел = %.2lf\n", avg_arith);
         printf("Середнє геометричне цілих чисел = %.2lf\n", avg_geom);
     } else {
-        printf("Не було введено цілих ненульових чисел.\n");
+        printf("\nНе було введено цілих ненульових чисел.\n");
     }
 
     return 0;
